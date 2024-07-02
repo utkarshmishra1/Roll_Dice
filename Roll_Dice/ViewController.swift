7c5d00e786b0106c3c973e1c3c035133b65a25ea
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-07-02 23:49:30 +0530

    Initial Commit

---
+++ Roll_Dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roll_Dice
+//
+//  Created by utkarsh mishra on 02/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
